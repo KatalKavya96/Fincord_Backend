13f03ffda2efab6e80b7bf815e3268747f21aa09
Kavya Katal <kavyakatal09@gmail.com>
2026-01-21 00:20:03 +0530

    db

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express")
+const app = express()
+
+const PORT = process.env.PORT
+
+app.get("/",(req,res)=>{
+
+    
+})
+
+app.listen(PORT,()=>{
+    console.log("Yay we are runnning!")
+})
